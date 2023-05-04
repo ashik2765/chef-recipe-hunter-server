4e7bb2bb8c07288211cc8fd425390b03f2a369f0
Ashikur Rahman <ashikurrahman2765@gmail.com>
2023-05-04 13:27:54 +0600

    send unique data from chef

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
 
 const chefs = require('./data/chefsData.json');
+
+app.use(cors());
 app.get('/',(req,res) =>{
     res.send('tabbakh resturant server is running')
 });
@@ -10,6 +13,12 @@ app.get('/',(req,res) =>{
 app.get('/chefs', (req,res) =>{
     res.send(chefs);
 })
+app.get('/chefs/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedChefs = chefs.find(n => n.id == id);
+    res.send(selectedChefs)
+})
+
 
 app.listen(port,()=>{
     console.log(`tabbakh API is running on port:${port}`)
